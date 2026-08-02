05d1b5c69d15f42a3a4da23bfe2bd1cb0dbdc5db
Maxim <darlinggbm@gmail.com>
2026-08-02 15:33:03 +0300

    feat: add Django app containerization and Helm chart deployment

--- main.tf
+++ main.tf
@@ -24,11 +24,18 @@ module "vpc" {
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   availability_zones = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  vpc_name           = "lesson-5-vpc"
+  vpc_name           = "lesson-7-vpc"
 }
 
 module "ecr" {
   source       = "./modules/ecr"
-  ecr_name     = "lesson-5-ecr"
+  ecr_name     = "lesson-7-ecr"
   scan_on_push = true
+}
+
+module "eks" {
+  source       = "./modules/eks"
+  cluster_name = "lesson-7-eks"
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.public_subnet_ids
 }
--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "vpc_id" {
 output "ecr_repository_url" {
   value = module.ecr.repository_url
 }
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
